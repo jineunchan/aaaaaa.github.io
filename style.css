23dd4cd2b842954ab6773d5cf5aa24152a6a5976
EnchanteJin <71077334+jineunchan@users.noreply.github.com>
2025-05-13 22:19:22 +0900

    Create style.css

---
+++ style.css
@@ -0,0 +1,14 @@
+body {
+    margin: 0;
+    overflow: hidden; /* 스크롤바 제거 */
+    background-color: #333;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+}
+
+#gameCanvas {
+    border: 1px solid black;
+    background-color: #77b5fe; /* 하늘색 배경 */
+}
